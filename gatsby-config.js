12974f9e22f48159685c0b473c420aabf275d28a
Keung123 <Jerry31415926535@gmail.com>
2024-01-16 13:08:06 +0800

    fix configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `JSB PCE Technology`,
+    description: `Meet the pinnacle of innovation in the world of Concrete Water Reducers and Equipment manufacturing.`,
+    author: `@emopp`,
+    siteUrl: `https://jsbpce.com/`,
   },
   plugins: [
     `gatsby-plugin-image`,
