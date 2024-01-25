153e29c68155dfa44ac052a8b4756ebe2d48a1a8
EmoPP <Jerry31415926535@gmail.com>
2024-01-26 02:04:08 +1000

    clean code & basic fb integration

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,42 @@
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
- */
-
-/**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  setHeadComponents([
+    <script
+      key="fb-sdk"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml            : true,
+              version          : 'v19.0'
+            });
+          };
+
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        `,
+      }}
+    />,
+  ]);
+
+  setPostBodyComponents([
+    <div key="fb-root" id="fb-root" />,
+    <div
+      key="fb-customer-chat"
+      id="fb-customer-chat"
+      className="fb-customerchat"
+      page_id="236075412914941"
+      attribution="biz_inbox"
+    />,
+  ]);
+};
